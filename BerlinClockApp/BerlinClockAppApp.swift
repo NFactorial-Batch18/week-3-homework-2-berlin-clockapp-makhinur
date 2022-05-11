473fc35c17289763e1ab01860cef6ae89c4c6095
MT <makhinurt@gmail.com>
2022-05-11 22:54:18 +0600

    Initial Commit

---
+++ BerlinClockApp/BerlinClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BerlinClockAppApp.swift
+//  BerlinClockApp
+//
+//  Created by Makhinur Talgatova on 11.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct BerlinClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
